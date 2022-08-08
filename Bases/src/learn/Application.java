dddf1747356e4bde230a762a08c0e9b82ae94270
matthcol <matthias.colin@gmail.com>
2022-08-08 11:05:55 +0200

    first commit

---
+++ Bases/src/learn/Application.java
@@ -0,0 +1,12 @@
+package learn;
+
+/**
+ * classe executable learn.Application
+ *
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Je démarre en Java");
+	}
+}
---
+++ Bases/src/learn/test/TestTexte.java
@@ -0,0 +1,14 @@
+package learn.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTexte {
+
+    @Test
+    void test1() {
+        System.out.println("Hello");
+    }
+
+}
