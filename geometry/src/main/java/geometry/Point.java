478053021d76795baa0a5be58d5f9d49f6039c5e
matthcol <matthias.colin@gmail.com>
2022-08-10 10:48:56 +0200

    test distance + translate

---
+++ geometry/src/main/java/geometry/Point.java
@@ -0,0 +1,58 @@
+package geometry;
+
+public class Point {
+
+	// attributes/fields
+	private String name;
+	private double x;
+	private double y;
+	
+	public Point() {
+	}
+	
+	public Point(String name, double x, double y) {
+		super();
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String getName() {
+		return name;
+		// i.e.:
+		// return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		// TODO: utiliser un StringBuilder si trop de concaténation
+		return name + "(" + x + "," + y + ")";
+	}
+	
+	// distance
+	public double distance(Point other) {
+		return 0.0;
+	}
+	
+	// translate
+	
+}
---
+++ geometry/src/test/java/geometry/test/TestPoint.java
@@ -0,0 +1,55 @@
+package geometry.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import geometry.Point;
+
+class TestPoint {
+
+	@Test
+	void testCreerDefaut() {
+		// create 2 instances (2 objects) of class Point
+		Point ptA = new Point();
+		Point ptB = new Point();
+		System.out.println(ptA);
+		System.out.println(ptB);
+		System.out.println(ptA.getClass());
+		System.out.println(ptA.toString());
+		System.out.println(ptA.getName() 
+				+ ": " + ptA.getX()
+				+ ","  + ptA.getY());
+		ptA.setX(3.2);
+		System.out.println(ptA.getName() 
+				+ ": " + ptA.getX()
+				+ ","  + ptA.getY());
+		
+	}
+	
+	@Test
+	void testCreerComplet() {
+		var ptA = new Point("A", 1.2, 3.4);
+		System.out.println(ptA);
+		System.out.println(ptA.getName() 
+				+ ": " + ptA.getX()
+				+ ","  + ptA.getY());
+	}
+	
+	@Test
+	void testDistance() {
+		var ptA = new Point("A", 1.25, 3.5);
+		var ptB = new Point("B", 4.25, 7.5);
+		var d = ptA.distance(ptB);
+		assertEquals(5.0, d);
+	}
+	
+	@Test
+	void testTranslate() {
+		var ptA = new Point("A", 1.25, 3.5);
+		ptA.translate(2.25, -1.25);
+		assertEquals(3.5, ptA.getX());
+		assertEquals(2.25, ptA.getY());
+	}
+
+}
