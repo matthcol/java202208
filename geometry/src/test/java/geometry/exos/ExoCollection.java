5b237bcbbd49ee95a7f85e4ab6484efdd98fb3ce
matthcol <matthias.colin@gmail.com>
2022-08-10 11:22:43 +0200

    exos collection point

---
+++ geometry/src/test/java/geometry/exos/ExoCollection.java
@@ -0,0 +1,44 @@
+package geometry.exos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ExoCollection {
+
+	@Test
+	void exoTranslateAll() {
+		// 1. creer une liste avec les points
+		// A(-1.1, 2.2)
+		// B(0.0, 3.3)
+		// C(1.1, 4.4)
+		// D(0.0, 5.5)
+		// E(3.3, 6.6)
+		// F(4.4, 3.3)
+		// 2. afficher la liste de points
+		// 3. translater tous les points de (-1,+1)
+		// 4. afficher la liste de points
+	}
+	
+	@Test
+	void exoXstats() {
+		// 1. reprendre la liste de l'exercice exoTranslateAll
+		// 2. donner la coordonnée X max de tous les points
+		// 3. donner la coordonnée X min de tous les points
+		// 4. donner la coordonnée X moyenne de tous les points
+	}
+	
+	@Test
+	void exoDistanceTotale() {
+		// 1. reprendre la liste de l'exercice exoTranslateAll
+		// 2. calculer la distance de chaque point au point origine (0,0)
+		// 3. calculer la distance totale
+	}
+	
+	@Test
+	void exoTri() {
+		// 1. reprendre la liste de l'exercice exoTranslateAll
+		// 2. trier les points par X croissant
+		// 2. trier les points par X décroissant, puis par name
+	}
+}
