743276e9a0ea095bc466d05a9542eeb2e5575585
matthcol <matthias.colin@gmail.com>
2022-08-10 12:14:05 +0200

    sol exo1 collection points

--- geometry/src/test/java/geometry/exos/ExoCollection.java
+++ geometry/src/test/java/geometry/exos/ExoCollection.java
@@ -2,7 +2,11 @@ package geometry.exos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import geometry.Point;
 
 public class ExoCollection {
 
@@ -15,14 +19,33 @@ public class ExoCollection {
 		// D(0.0, 5.5)
 		// E(3.3, 6.6)
 		// F(4.4, 3.3)
+		List<Point> points = List.of(
+				new Point("A",-1.1, 2.2),
+				new Point("B",0.0, 3.3),
+				new Point("C",1.1, 4.4),
+				new Point("D",0.0, 5.5),
+				new Point("E",3.3, 6.6),
+				new Point("F",4.4, 3.3)
+			);
 		// 2. afficher la liste de points
+		System.out.println(points);
+		for (var pt: points) {
+			System.out.println(" - " + pt);
+		}
 		// 3. translater tous les points de (-1,+1)
+		for (var pt: points) {
+			pt.translate(-1, 1);
+		}
 		// 4. afficher la liste de points
+		System.out.println(points);
 	}
 	
 	@Test
 	void exoXstats() {
 		// 1. reprendre la liste de l'exercice exoTranslateAll
+		List<Point> points = List.of(
+				new Point("A",-1.1, 2.2)
+				);
 		// 2. donner la coordonnée X max de tous les points
 		// 3. donner la coordonnée X min de tous les points
 		// 4. donner la coordonnée X moyenne de tous les points
