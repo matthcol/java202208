65c18ae349a84affd13e3282255c96a0b434b661
matthcol <matthias.colin@gmail.com>
2022-08-10 12:34:07 +0200

    sol exo2 3/4 collection points

--- geometry/src/test/java/geometry/exos/ExoCollection.java
+++ geometry/src/test/java/geometry/exos/ExoCollection.java
@@ -44,11 +44,30 @@ public class ExoCollection {
 	void exoXstats() {
 		// 1. reprendre la liste de l'exercice exoTranslateAll
 		List<Point> points = List.of(
-				new Point("A",-1.1, 2.2)
-				);
+				new Point("A",-1.1, 2.2),
+				new Point("B",0.0, 3.3),
+				new Point("C",1.1, 4.4),
+				new Point("D",0.0, 5.5),
+				new Point("E",3.3, 6.6),
+				new Point("F",4.4, 3.3)
+			);
 		// 2. donner la coordonnée X max de tous les points
+		double xMax = Double.NEGATIVE_INFINITY; // -inf
+		for (var pt: points) {
+			if (pt.getX() > xMax) {
+				xMax = pt.getX(); 
+			}
+		}
+		System.out.println("X max: " + xMax);
 		// 3. donner la coordonnée X min de tous les points
 		// 4. donner la coordonnée X moyenne de tous les points
+		// (Σx_i)/nb = Σ(x_i/nb)
+		double xSum = 0.0;
+		for (var pt: points) {
+			xSum += pt.getX();
+		}
+		double xAvg = xSum / points.size();
+		System.out.println("X average: " + xAvg);
 	}
 	
 	@Test
